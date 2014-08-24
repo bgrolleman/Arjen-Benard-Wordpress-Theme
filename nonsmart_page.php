4246665133ccfbe7f2087edf06043cbd8111ac25
Bas Grolleman <bgrolleman@emendo-it.nl>
2014-08-24 10:50:07 +0200

    Adding last changes

---
+++ nonsmart_page.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Non-Smart */ ?>
+<?php get_header(); ?>
+  <div class="allContent">
+	<?php 
+			get_template_part ( 'theloop' );
+	?>
+    <br class="clearfloat" />
+<?php get_footer() ?>
